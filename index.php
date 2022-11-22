7c58abb428d0e65de0e5dc1b694d93a47b5fcb7f
JoseTorres022 <joseangel.tosa@outlook.com>
2022-11-22 00:00:08 -0700

    tarea lisata

--- index.php
+++ index.php
@@ -89,15 +89,34 @@
 
                         <td>
                             <!-- CRUD (UPDATE): Generando la consulta necesaria para la operacion basica (3) "UPDATE": -->
-                            <a href="actualizarDatos.php?numero_Control=<?php echo $row['numero_Control'] ?>">
-                            <i class="fa-light fa-pen-to-square"></i>
+                            <a style="text-decoration:none; margin: 1px;" href="actualizarDatos.php?numero_Control=<?php echo $row['numero_Control'] ?>">
+                                <!-- <i class="fa-light fa-pen-to-square"></i> -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit"
+                                    width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="#000000"
+                                    fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
+                                    <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
+                                    <line x1="16" y1="5" x2="19" y2="8" />
+                                </svg>
                             </a>
                             <!-- Fin de la operacion basica UPDATE -->
-
+                                <a style="font-size: 15px;">
+                                    |
+                                </a>
                             <!-- CRUD (DELETE): Generando la consulta necesaria para la operacion basica (3) "UPDATE": -->
-                            <a href="elminarDatos.php?numero_Control=<?php echo $row['numero_Control'] ?>">
-                                eliminar
-
+                            <a style="text-decoration:none; margin: 1px;" href="elminarDatos.php?numero_Control=<?php echo $row['numero_Control'] ?>">
+                                <!-- eliminar -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash"
+                                    width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="#000000"
+                                    fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <line x1="4" y1="7" x2="20" y2="7" />
+                                    <line x1="10" y1="11" x2="10" y2="17" />
+                                    <line x1="14" y1="11" x2="14" y2="17" />
+                                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                </svg>
                             </a>
                             <!-- Fin de la operacion basica DELETE -->
 
